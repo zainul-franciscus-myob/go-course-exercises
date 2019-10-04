054841dcde1f33e29c1413079bf8f90ff4c5073e
Zainul Franciscus <Zainul.Franciscus@myob.com>
2019-10-05 08:36:33 +1000

    my own answers for exercise 2

---
+++ Exercises - Ninja Level 1/Hands-on exercise #2/practice.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//append a number behind each of this variable name so that the var names does not overlap with the ones in main.go
+var x1 int
+var y2 int
+var z3 int
+
+func main () {
+	fmt.Println(x,y,z)
+}
